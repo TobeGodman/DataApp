dff6de8b5b098b44c7c370713a516d4c74cf7ec7
金海天 <haitian299@gmail.com>
2015-03-24 16:07:52 +0800

    首次上传

---
+++ DataApp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DataApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectBt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.getDataBt = new System.Windows.Forms.Button();
+            this.TickDataGridView = new System.Windows.Forms.DataGridView();
+            this.StopBt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TickDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectBt
+            // 
+            this.connectBt.Location = new System.Drawing.Point(31, 12);
+            this.connectBt.Name = "connectBt";
+            this.connectBt.Size = new System.Drawing.Size(95, 23);
+            this.connectBt.TabIndex = 0;
+            this.connectBt.Text = "连接服务器";
+            this.connectBt.UseVisualStyleBackColor = true;
+            this.connectBt.Click += new System.EventHandler(this.connectBt_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(595, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(223, 305);
+            this.textBox1.TabIndex = 2;
+            // 
+            // getDataBt
+            // 
+            this.getDataBt.Location = new System.Drawing.Point(31, 41);
+            this.getDataBt.Name = "getDataBt";
+            this.getDataBt.Size = new System.Drawing.Size(95, 23);
+            this.getDataBt.TabIndex = 3;
+            this.getDataBt.Text = "获取行情";
+            this.getDataBt.UseVisualStyleBackColor = true;
+            this.getDataBt.Click += new System.EventHandler(this.getDataBt_Click);
+            // 
+            // TickDataGridView
+            // 
+            this.TickDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TickDataGridView.Location = new System.Drawing.Point(31, 107);
+            this.TickDataGridView.Name = "TickDataGridView";
+            this.TickDataGridView.RowHeadersVisible = false;
+            this.TickDataGridView.RowTemplate.Height = 23;
+            this.TickDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.TickDataGridView.Size = new System.Drawing.Size(558, 376);
+            this.TickDataGridView.TabIndex = 4;
+            // 
+            // StopBt
+            // 
+            this.StopBt.Location = new System.Drawing.Point(31, 70);
+            this.StopBt.Name = "StopBt";
+            this.StopBt.Size = new System.Drawing.Size(95, 23);
+            this.StopBt.TabIndex = 5;
+            this.StopBt.Text = "Stop";
+            this.StopBt.UseVisualStyleBackColor = true;
+            this.StopBt.Click += new System.EventHandler(this.StopBt_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 506);
+            this.Controls.Add(this.StopBt);
+            this.Controls.Add(this.TickDataGridView);
+            this.Controls.Add(this.getDataBt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.connectBt);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "行情";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TickDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button getDataBt;
+        private System.Windows.Forms.DataGridView TickDataGridView;
+        private System.Windows.Forms.Button StopBt;
+    }
+}
+
---
+++ DataApp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QuantBox.XAPI.Callback;
+using QuantBox.XAPI;
+using QuantBox;
+using System.Diagnostics;
+
+namespace DataApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private XApi api;
+
+        DataTable tickDataTable = new DataTable();
+
+
+
+
+
+
+
+        /// <summary>
+        /// OnConnectionStatus回调
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="status"></param>
+        /// <param name="userLogin"></param>
+        /// <param name="size1"></param>
+        private void OnConnectionStatus(object sender, ConnectionStatus status, ref RspUserLoginField userLogin, int size1)
+        {
+            showInfo("showing info inside onConnectionStatus now");
+            showInfo("//" + status + userLogin.ErrorMsg());
+            
+        }
+
+        /// <summary>
+        /// 深度行情回调
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="marketData"></param>
+
+        private void OnRtnDepthMarketData(object sender, ref DepthMarketDataField marketData)
+        {
+            showInfo("showing info inside depth now");
+            showInfo("//" + marketData.InstrumentID);
+            showInfo("//" + marketData.ExchangeID);
+            showInfo("//" + marketData.LastPrice.ToString());
+
+            var row = tickDataTable.NewRow();
+
+            foreach (var fi in typeof(DepthMarketDataField).GetFields())
+            {
+                switch (fi.Name)
+                {
+                    case "InstrumentID" :
+                        row[fi.Name] = marketData.InstrumentID;
+                        break;
+                }
+            }
+
+        }
+
+        
+        /// <summary>
+        /// 连接行情前置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
+        private void connectBt_Click(object sender, EventArgs e)
+        {
+            showInfo("begin");
+            showInfo("Initialize api");
+            api = new XApi(@"C:\Users\jht\Documents\Visual Studio 2013\Projects\DataApp\DataApp\dll\QuantBox_CTP_Quote.dll");
+            showInfo("Initialize server info");
+            api.Server.BrokerID = "66666";
+            api.Server.Address = "tcp://ctp1-md9.citicsf.com:41213";
+
+            showInfo("set 回调函数");
+            api.OnConnectionStatus += OnConnectionStatus;
+            api.OnRtnDepthMarketData += OnRtnDepthMarketData;
+
+            showInfo("begin connecting");
+            api.Connect();
+            
+
+            //Thread.Sleep(3000);
+            //showInfo("begin dispose");
+            //api.Dispose();
+
+           //Thread.Sleep(5 * 1000);
+        }
+
+        /// <summary>
+        /// 订阅行情
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void getDataBt_Click(object sender, EventArgs e)
+        {
+            //showInfo("begin subscribe quotes");
+            api.Subscribe("IF1504", "");
+        }
+        
+        private void showInfo(string info)
+        {
+            this.Invoke(new Action(() =>
+            {
+                this.textBox1.Text += info + System.Environment.NewLine;
+                this.textBox1.SelectionStart = this.textBox1.TextLength;
+                this.textBox1.ScrollToCaret();
+            }));
+        }
+
+        /// <summary>
+        /// 初始化dataview窗口
+        /// </summary>
+        private void InitDataView()
+        {
+            
+            Type t = typeof(DepthMarketDataField);
+            string keyColumn = "UpdateTime,TradingDay,InstrumentID,ExchangeID";
+            if (t == null)
+                return;
+            foreach (var v in t.GetFields())
+            {
+                tickDataTable.Columns.Add(v.Name, v.FieldType);
+            }
+            tickDataTable.PrimaryKey = keyColumn.Split(',').Select(v => tickDataTable.Columns[v]).ToArray();
+
+            this.TickDataGridView.DataSource = tickDataTable;
+
+
+
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            InitDataView();
+
+        }
+
+        /// <summary>
+        /// 停止行情
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopBt_Click(object sender, EventArgs e)
+        {
+            api.Dispose();
+        }
+        
+    }
+}
